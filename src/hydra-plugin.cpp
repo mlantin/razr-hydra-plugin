b9d70b7e930df9548f41fa2077882ded453d333a
mlantin <maria.lantin@gmail.com>
2016-05-07 18:21:40 -0700

    changed it to work with simple websocket and the new protobuf message format.

--- src/hydra-plugin.cpp
+++ src/hydra-plugin.cpp
@@ -1,13 +1,9 @@
 #include "sixense/sixense.h"
-<<<<<<< HEAD
-#include "VRCom.pb.h"
-#include "sioclient/include/sio_client.h"
+#include "protobuf/VRCom.pb.h"
+#include "easywsclient.hpp"
 
 #include <iostream>
-=======
-#include "easywsclient.hpp"
 #include <cassert>
->>>>>>> simplesocket
 #include <sstream>
 #include <chrono>
 #include <thread>
@@ -16,10 +12,7 @@ int main() {
 
 	using easywsclient::WebSocket;
 	WebSocket::pointer ws = NULL;
-	std::ostringstream oss;
-	oss.precision(7);
 	
-
 	sixenseControllerData data;
 	sixenseInit();
 	
@@ -28,16 +21,17 @@ int main() {
 	while (!(numctrl = sixenseGetNumActiveControllers()))
 		;
 
-<<<<<<< HEAD
 	std::ostringstream msgbuf;
+	VRCom::Update* updateMsg = new VRCom::Update();
 	VRCom::Hydra* msg = new VRCom::Hydra();
+	updateMsg->set_allocated_hydra(msg);
 	VRCom::Joystick* joy = new VRCom::Joystick();
 	VRCom::Position* pos = new VRCom::Position();
 	VRCom::Rotation* rot = new VRCom::Rotation();
 	msg->set_allocated_pos(pos);
 	msg->set_allocated_rot(rot);
 	msg->set_allocated_joy(joy);
-=======
+
     // open the websocket server connection
     #ifdef _WIN32
     INT rc;
@@ -50,26 +44,25 @@ int main() {
     }
     #endif
 
-    ws = WebSocket::from_url("ws://192.168.11.34:4567/socket.io/?EIO=4&transport=websocket");
+    ws = WebSocket::from_url("ws://192.168.11.34:4567");
     assert(ws);
->>>>>>> simplesocket
 
 	while(true) {
 		for (int i = 0; i < numctrl; i++) {
 			sixenseGetNewestData(i, &data);
-<<<<<<< HEAD
-			std::cerr << "controller: " << i << std::endl;
-			std::cerr << "pos: " << data.pos[0] << ", " << data.pos[1] << ", " << data.pos[2] << std::endl;
-			std::cerr << "rot: " << data.rot_quat[0] << ", " << data.rot_quat[1] << ", " << data.rot_quat[2] << ", " << data.rot_quat[3] << std::endl;
-			std::cerr << "joystick: " << data.joystick_x << ", " << data.joystick_y << std::endl;
-			std::cerr << "trigger: " << data.trigger << std::endl;
-			std::cerr << "buttons: " << (data.buttons&SIXENSE_BUTTON_BUMPER) << " "
-									<< (data.buttons&SIXENSE_BUTTON_JOYSTICK) << " "
-									<< (data.buttons&SIXENSE_BUTTON_1) << " "
-									<< (data.buttons&SIXENSE_BUTTON_2) << " "
-									<< (data.buttons&SIXENSE_BUTTON_3) << " "
-									<< (data.buttons&SIXENSE_BUTTON_4) << " "
-									<< (data.buttons&SIXENSE_BUTTON_START) << std::endl;
+
+			// std::cerr << "controller: " << i << std::endl;
+			// std::cerr << "pos: " << data.pos[0] << ", " << data.pos[1] << ", " << data.pos[2] << std::endl;
+			// std::cerr << "rot: " << data.rot_quat[0] << ", " << data.rot_quat[1] << ", " << data.rot_quat[2] << ", " << data.rot_quat[3] << std::endl;
+			// std::cerr << "joystick: " << data.joystick_x << ", " << data.joystick_y << std::endl;
+			// std::cerr << "trigger: " << data.trigger << std::endl;
+			// std::cerr << "buttons: " << (data.buttons&SIXENSE_BUTTON_BUMPER) << " "
+			// 						<< (data.buttons&SIXENSE_BUTTON_JOYSTICK) << " "
+			// 						<< (data.buttons&SIXENSE_BUTTON_1) << " "
+			// 						<< (data.buttons&SIXENSE_BUTTON_2) << " "
+			// 						<< (data.buttons&SIXENSE_BUTTON_3) << " "
+			// 						<< (data.buttons&SIXENSE_BUTTON_4) << " "
+			// 						<< (data.buttons&SIXENSE_BUTTON_START) << std::endl;
 
 			msg->set_ctrl_num(i);
 			pos->set_x(data.pos[0]);
@@ -84,20 +77,14 @@ int main() {
 			msg->set_trigger(data.trigger);
 			msg->set_buttons(data.buttons);
 
-			msg->SerializeToOstream(&msgbuf);
-			ws.socket()->emit("hydra", std::make_shared<std::string>(msgbuf.str()));
+			updateMsg->SerializeToOstream(&msgbuf);
+			ws->sendBinary(msgbuf.str());
+			ws->poll();
+
 			std::ostringstream().swap(msgbuf);
           	msgbuf.clear();
-=======
-			oss << "42[\"obj\",{\"hydra" << i << "\":[";
-			oss << data.pos[0] << ",";
-			oss << data.pos[1] << ",";
-			oss << data.pos[2] << "]}]";
-			ws->send(oss.str());
-			ws->poll();
-			std::ostringstream().swap(oss);
->>>>>>> simplesocket
-			std::this_thread::sleep_for(std::chrono::milliseconds(15));
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(20));
 			// msg.reset();
 			//std::cerr << data.pos[0] << std::endl;
 			//ws->send("5:1::{\"name\":\"hydra\",\"args\":\"bla\"}");
